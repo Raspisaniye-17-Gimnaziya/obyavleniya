fb7f9bf31105e95d7cd8844881d0d38a6ddff52c
Raspisaniye-17-Gimnaziya <91901163+Raspisaniye-17-Gimnaziya@users.noreply.github.com>
2021-11-22 21:43:55 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn7v = document.querySelectorAll('.tab button')[2];
+btn7v.addEventListener('click', ()=> {
+    let password = prompt('Введите пароль');
+         if ((password=="177317" && password!=null && password!="") && password!=undefined && password!=false){
+            
+         }
+         else {document.location.href = "https://www.google.com/search?q=%D0%94%D0%BE%D1%81%D0%B2%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5!&oq=%D0%94%D0%BE%D1%81%D0%B2%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5!&aqs=chrome..69i57.11596j0j7&sourceid=chrome&ie=UTF-8";}
+});
